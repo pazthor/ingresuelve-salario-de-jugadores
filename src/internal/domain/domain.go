2dfdd5fc47818fae8c8bfdd9b5a6a394455e9512
Julio Pastor <hi.pazthor@gmail.com>
2020-10-02 00:26:30 -0500

    Se agrega funciones para obtener el salario de jugadores

--- src/internal/domain/domain.go
+++ src/internal/domain/domain.go
@@ -23,3 +23,75 @@ type Jugador struct {
 	Equipo         string  `json:"equipo"`
 	GolesMinimo    int     `json:"goles_minimos"`
 }
+
+//CalcularTotalDeGoles show
+func (jugadores *Jugadores) CalcularTotalDeGoles() {
+
+	var totalGolesEstimados = 0
+	var totalGolesMetidos = 0
+
+	for _, v := range jugadores.Jugadores {
+		totalGolesMetidos += v.Goles
+		golesEstimados := getNivelDeGoles(v.Nivel)
+		totalGolesEstimados += golesEstimados
+	}
+
+	jugadores.TotalGolesEstimados = totalGolesEstimados
+	jugadores.TotalGolesMetidos = totalGolesMetidos
+}
+
+//CalcularSalariosPorJugador actualiza los salarios correspondientes
+// para cada jugador
+func (jugadores *Jugadores) CalcularSalariosPorJugador() {
+
+	totalDeGolesEstimados := float32(jugadores.TotalGolesEstimados)
+
+	tam := len(jugadores.Jugadores)
+	for i := 0; i < tam; i++ {
+		bonoPorEquipo := jugadores.Jugadores[i].CalcularBonoPorEquipo(float32(jugadores.TotalGolesMetidos), totalDeGolesEstimados, jugadores.getPesoBonoPorEquipo())
+		jugadores.Jugadores[i].SueldoCompleto = jugadores.Jugadores[i].calcularSueldoCompleto(bonoPorEquipo)
+
+	}
+}
+
+//CalcularBonoPorEquipo return float
+func (jugador *Jugador) CalcularBonoPorEquipo(golesMetidos, golesEstimados, peso float32) (result float32) {
+
+	bono := peso * jugador.Bono
+
+	promedio := golesMetidos / golesEstimados
+	result = bono * promedio
+	return
+}
+func (jugador *Jugador) calcularSueldoCompleto(bonoPorEquipo float32) float32 {
+	bonoIndividual := jugador.BonoIndividual()
+	bonoTotal := bonoIndividual + bonoPorEquipo
+	sueldoTotal := jugador.SueldoBase + bonoTotal
+
+	return sueldoTotal
+}
+
+//BonoIndividual return float
+func (jugador *Jugador) BonoIndividual() (result float32) {
+
+	golesEstimados := getNivelDeGoles(jugador.Nivel)
+	porcentajeGoles := float32(jugador.Goles) / float32(golesEstimados)
+
+	bono := jugador.getPesoBonoIndividual() * jugador.Bono
+
+	result = bono * porcentajeGoles
+
+	return
+}
+
+func (jugadores *Jugadores) getPesoBonoPorEquipo() float32 {
+	return 0.5
+}
+
+func (jugador *Jugador) getPesoBonoIndividual() float32 {
+	return 0.5
+}
+func getNivelDeGoles(s string) int {
+	var nivelJugador = map[string]int{"A": 5, "B": 10, "C": 15, "Cuauh": 20}
+	return nivelJugador[s]
+}
