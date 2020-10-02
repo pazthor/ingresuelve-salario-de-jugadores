7d14ab16356ee5d75328a22ebf1539291a6200ab
Julio Pastor <hi.pazthor@gmail.com>
2020-10-02 00:28:36 -0500

    Se llaman las funciones para actualizar los salarios

--- src/internal/service/service.go
+++ src/internal/service/service.go
@@ -8,6 +8,8 @@ import (
 //Proceso realiza las operaciones para completar el reto
 func Proceso() *domain.Jugadores {
 	equipo := repository.GetJugadores()
+	equipo.CalcularTotalDeGoles()
+	equipo.CalcularSalariosPorJugador()
 
 	return equipo
 
